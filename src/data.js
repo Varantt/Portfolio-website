5a3f530622d3b9991063d10be197cbfebc1914d4
Varantt <varantkalemkerian@gmail.com>
2023-02-09 23:10:20 +0200

    another path change

--- src/data.js
+++ src/data.js
@@ -6,7 +6,7 @@ export default [
     stack: ["HTML", "Bootstrap", "js/jQuery"],
     description:
       "My first “big” project was composed of: HTML, CSS, and JS. The latter was a university project meant to develop my JS and jQuery skills.  ",
-    imgUrl: "./photos/resto.png",
+    imgUrl: "/photos/resto.png",
     link: "https://Varantt.github.io",
   },
   {
@@ -14,7 +14,7 @@ export default [
     stack: ["Wordpress", "CSS"],
     description:
       "This website was my first Wordpress project, where I learned the major concepts in Wordpress. It was challenging at first, but my dedication to it made the learning process enjoyable. Currently, I’ve mastered most of the skills needed to prepare a Wordpress site.",
-    imgUrl: "../photos/hairenik.png",
+    imgUrl: "/photos/hairenik.png",
     link: "https://hairenikweekly.com",
   },
   {
@@ -22,7 +22,7 @@ export default [
     stack: ["Wordpress", "Elementor"],
     description:
       "Through this project, i learned concepts about Elementor, where i used attractive widgets to create a basic website for a relative. This site might be a little outdated, but is one of my few live sites that i have. ",
-    imgUrl: "photos/kitchen.png",
+    imgUrl: "/photos/kitchen.png",
     link: "https://latilesandkitchen.com",
   },
 
@@ -49,7 +49,7 @@ export default [
     stack: ["HTML, CSS, JS"],
     description:
       "This project was a game to guess the correct color given some RGB values. I also learned some crucial JS skills, like DOM manipulation, functions, callbacks, etc.",
-    imgUrl: "./photos/colorgame.png",
+    imgUrl: "/photos/colorgame.png",
     link: "https://github.com/Varantt/Color-game-project",
   },
 ];
