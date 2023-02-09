45f3c5a26cb2c2222e161f269a4d7201314c794b
Varantt <varantkalemkerian@gmail.com>
2023-02-09 18:40:17 +0200

    path fix 5

--- src/data.js
+++ src/data.js
@@ -4,7 +4,7 @@ export default [
     stack: ["HTML", "Bootstrap", "js/jQuery"],
     description:
       "My first “big” project was composed of: HTML, CSS, and JS. The latter was a university project meant to develop my JS and jQuery skills.  ",
-    imgUrl: "../src/assets/resto.png",
+    imgUrl: "https://varantkalemkerian.netlify.app/photos/resto.png",
     link: "https://Varantt.github.io",
   },
   {
@@ -12,7 +12,7 @@ export default [
     stack: ["Wordpress", "CSS"],
     description:
       "This website was my first Wordpress project, where I learned the major concepts in Wordpress. It was challenging at first, but my dedication to it made the learning process enjoyable. Currently, I’ve mastered most of the skills needed to prepare a Wordpress site.",
-    imgUrl: "./photos/kitchen.png",
+    imgUrl: "/photos/hairenik.png",
     link: "https://hairenikweekly.com",
   },
   {
@@ -20,8 +20,8 @@ export default [
     stack: ["Wordpress", "Elementor"],
     description:
       "Through this project, i learned concepts about Elementor, where i used attractive widgets to create a basic website for a relative. This site might be a little outdated, but is one of my few live sites that i have. ",
-    imgUrl: "../src/assets/kitchen.png",
-    link: "https://github.com/Varantt/Tic-Tac-Toe-X-O-",
+    imgUrl: "photos/kitchen.png",
+    link: "https://latilesandkitchen.com",
   },
 
   {
@@ -29,7 +29,7 @@ export default [
     stack: ["React", "CSS"],
     description:
       "This React project stands out among my many others as it showcases a significant display of my skills, having provided ample opportunity for me to put into practice the mastery I have attained.  ",
-    imgUrl: "../src/assets/cocktail.png",
+    imgUrl: "/photos/cocktail.png",
     link: "https://test-react-project-2.netlify.app/",
   },
 
@@ -38,7 +38,7 @@ export default [
     stack: ["HTML", "Bootstrap", "PHP", "mySql"],
     description:
       "This was my Final Project in the Lebanese University, where I worked with php and mySqli to connect to a local database. Throughout the learning journey, I acquired some of the basic backend and Front-end principles. Additionally, I learned the foundational js concepts, such as function, css-in-js, etc.  ",
-    imgUrl: "../src/assets/charity.png",
+    imgUrl: "/photos/charity.png",
     link: "https://github.com/Varantt/charity-website",
   },
 
@@ -47,7 +47,7 @@ export default [
     stack: ["HTML, CSS, JS"],
     description:
       "This project was a game to guess the correct color given some RGB values. I also learned some crucial JS skills, like DOM manipulation, functions, callbacks, etc.",
-    imgUrl: "../src/assets/colorgame.png",
+    imgUrl: "./photos/colorgame.png",
     link: "https://github.com/Varantt/Color-game-project",
   },
 ];
