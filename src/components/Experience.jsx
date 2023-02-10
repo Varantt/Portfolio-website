4d5cdb42545b0b8a9bac04365259a3cfebff40cf
Varantt <varantkalemkerian@gmail.com>
2023-02-10 20:38:43 +0200

    small fix

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function Experience({ name, description, type, startDate, endDate }) {
   return (
-    <section id="experience ">
-      <div className="line-height-{20px}">
+    <section id="experience">
+      <div className="">
         <div className="text-lg md:text-3xl text-white pl-6">{name}</div>
         <div className="text-sm md:text-md text-gray-500 pl-6">
           {startDate} - {endDate}
