fc36a1e79dce3a4850735030d51dfb589b450626
Varantt <varantkalemkerian@gmail.com>
2023-02-15 23:17:15 +0200

    comma fix

--- src/e-data.js
+++ src/e-data.js
@@ -12,7 +12,7 @@ export const eData = [
   {
     name: "Wordpress Developer",
     description: [
-      "- Updated the company’s website, using Wordpress. ",
+      "- Updated the company’s website using Wordpress. ",
       "- Website maintenance.",
     ],
     type: "Part time",
