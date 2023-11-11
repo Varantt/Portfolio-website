de432ad2e11b21b5404c4142153a263445f2cff9
Varantt <varantkalemkerian@gmail.com>
2023-11-11 18:05:04 +0200

    added freelance project

--- src/data.js
+++ src/data.js
@@ -9,7 +9,7 @@ export default [
   },
   {
     title: "Spotify Artist Albums",
-    stack: ["ReactJs" , "TailwindCSS" , "Vite"],
+    stack: ["ReactJs", "TailwindCSS", "Vite"],
     description:
       "The Spotify Album Explorer is a web app that allows users to search for artists and browse through their available albums on Spotify. The app utilizes the Spotify Web API to retrieve data about artists and their albums, including album names, release dates, tracklists, and album artwork. Upon launching the app, the user can enter the name of an artist in the search bar to look up that artist's discography. The app displays a list of the artist's available albums, sorted by release date in descending order. Each album is represented by its cover art, album title, and release year. Clicking on an album opens a modal window that displays the album's tracklist, with each track listed in order and labeled with its duration.",
     imgUrl: "/assets/spotify.png",
@@ -24,16 +24,6 @@ export default [
     imgUrl: "/assets/hairenik.png",
     link: "https://hairenikweekly.com",
   },
-
-  {
-    title: "Cocktails List Project",
-    stack: ["ReactJs", "CSS"],
-    description:
-      "This React project stands out among my many others, as it showcases a significant display of my skills, having provided ample opportunity for me to put into practice the mastery I have attained.  ",
-    imgUrl: "/assets/cocktail.png",
-    link: "https://test-react-project-2.netlify.app/",
-  },
-
   {
     title: "Charity Website",
     stack: ["HTML", "Bootstrap", "PHP", "mySql"],
@@ -45,26 +35,25 @@ export default [
 
   {
     title: "Todo-list application",
-    stack: ["ReactJs" , "SCSS"],
+    stack: ["ReactJs", "SCSS"],
     description:
       "This project is a todo-list responsive react application, where i used my scss and React skills to create a useful application",
     imgUrl: "/assets/todo.png",
     link: "https://vktodolist.netlify.app",
   },
   {
-    title: "Wordpress Tiles and kitchen website",
+    title: "LA Tiles and Kitchen: Tiles and kitchen website",
     stack: ["Wordpress", "Elementor"],
     description:
-      "Throughout this project, i learned concepts about Elementor, where i created attractive widgets to create a basic website for a relative. This site might be a little outdated, but it is one of my few live ones. ",
+      "Throughout this project, I learned concepts about Elementor, where i created attractive widgets to create a basic website for a relative. This site might be a little outdated, but it is one of my few live ones. ",
     imgUrl: "/assets/kitchen.png",
     link: "https://latilesandkitchen.com",
   },
   {
-    title: "Wordpress: Information about a Public figure",
-    stack: ["Wordpress", "Elementor"],
-    description:
-      "In this project, I have presented the Talaat Pasha's report on the Armenian Genocide. This significant document sheds light on the planned and methodical elimination of the Armenian community in the Ottoman Empire during World War I. To make the content easy to navigate, I have categorized the information using posts and categories. To create an organized and visually appealing website, I utilized Elementor. ",
-    imgUrl: "/assets/talaat.png",
-    link: "https://talaatpasha.net/",
+    title: "The Corner: Real Estate Website",
+    stack: ["Wordpress", "WPBakery", "Custom Plugin"],
+    description: "In this project, I learned concepts about WPBakery and applied my wordpress development knowledge to match client's specific requirements.",
+    imgUrl: "/assets/thecorner.png",
+    link: "https://thecornerae.com/",
   },
 ];
