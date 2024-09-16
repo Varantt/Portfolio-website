7caa19e707b28ac61fb62138e7164a7f8a5fab82
Varantt <varant@itxi.net>
2024-09-16 23:57:55 +0300

    Adding variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import Home from "./components/Home";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar/Navbar";
 import Error from "./components/Error";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Project from "./components/Project";
+import Project from "./components/Project/Project";
 import Footer from "./components/Footer";
 import Message from "./components/Message";
 function App() {
-
   return (
-    <div className="app">
+    <div className="app ">
       <Router>
         <Navbar />
         <Routes>
