eea5886570ea7a935deede7a3864c0fc2fdfe7e4
Varantt <varant@itxi.net>
2024-09-22 20:27:08 +0300

    adding loading fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
-import React, { Suspense } from 'react'
-import ReactDOM from 'react-dom/client'
-import {Loading} from './components/Loading/Loading'
-import './index.css'
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom/client";
+import { Loading } from "./components/Loading/Loading";
+import "./index.css";
 
-const AsyncApp = React.lazy(() => import ('./App'));
-;
-
+const AsyncApp = React.lazy(
+  () =>
+    new Promise((resolve) => {
+      setTimeout(() => resolve(import("./App")), 1000);
+    })
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
